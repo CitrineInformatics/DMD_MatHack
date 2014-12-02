65cfcba6b0e83bed94350dbbf945864fa8fbbed3
imagdau <i.b.magdau@gmail.com>
2014-12-02 18:32:12 -0500

    corrected

--- loadData.m
+++ loadData.m
@@ -6,7 +6,7 @@ load perovskite.dat;
 prv = perovskite;
 elm = elem_data;
 input_data = zeros(size(prv,1), 2*size(elm,2)+1);
-solution_data = prv(4:end,:);
+solution_data = prv(:,4:end);
 
 for i=1:size(prv,1),
   input_data(i,:) = [elm(elm(:,1)==prv(i,1),:) elm(elm(:,1)==prv(i,2),:) prv(i,3)];
